71359a4ef9e3eca631f22e9b401cf08d3d59f7a7
reactdattu2021 <react.dattu2021@gmail.com>
2025-09-06 12:00:03 +0530

    chaged vercel url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: "https://clickks-assignment.vercel.app/",
   credentials: true
 }));
 app.use(session({
